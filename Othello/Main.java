a2c1fbfa74ddb604dc4a0d12b9596c75489e2151
MHasannejadi <hasannejadiam@gmail.com>
2020-06-16 15:51:26 +0430

    console version

---
+++ Othello/Main.java
@@ -0,0 +1,11 @@
+/**
+ * This class contains main function to run this program 
+ * @author Mohammad Hassannejadi
+ * @version 1.0
+ */
+public class Main{
+    public static void main(String[] args) {
+        Game othello = new Game();
+        othello.play();
+    }    
+}
